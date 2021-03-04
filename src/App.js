52767d20ea5bd2365e1ab063c68c51db73271bd1
SimonGPort <simon.giroux.portelance@gmail.com>
2021-03-03 22:25:12 -0500

    set Three.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import * as THREE from 'three';
 
-function App() {
+let camera, scene, renderer;
+let geometry, material, mesh;
+
+class App extends Component {
+
+componentDidMount(){
+  this.init()
+}
+
+init = () => {
+
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 1;
+
+  scene = new THREE.Scene();
+
+  geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  // renderer.setClearColor("#e5e5e5")
+	renderer.setSize( window.innerWidth, window.innerHeight );
+ 
+	renderer.setAnimationLoop( this.animation );
+	document.body.appendChild( renderer.domElement );
+}
+
+animation=(time)=>{
+  console.log("time:",time)
+  mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
+
+
+render = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ma page Three.js
     </div>
   );
+}
 }
 
 export default App;
